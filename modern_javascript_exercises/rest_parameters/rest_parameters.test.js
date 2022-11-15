3ced80e930a3731d999a340fd968315523683dc5
Ellie Judge <judge_ellie@hotmail.com>
2022-11-15 17:46:24 +0000

    (wip) Adds read mes to the different task folders

--- modern_javascript_exercises/rest_parameters/rest_parameters.test.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.test.js
@@ -1,6 +1,6 @@
 const add = require("./rest_parameters");
 
-test("it adds all the numbers passed in", () => {
+test("it adds together all the numbers in the argument", () => {
   expect(add(1, 2)).toBe(3);
-  //   expect(add(1, 2, 3, 4, 5)).toBe(15);
+  expect(add(1, 2, 3, 4, 5)).toBe(15);
 });
