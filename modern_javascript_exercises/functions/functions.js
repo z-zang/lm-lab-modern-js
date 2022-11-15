6e28a4bd2cb6f0ced3789fa18b54fe19107c6f8e
Ellie Judge <judge_ellie@hotmail.com>
2022-11-15 19:34:12 +0000

    adds exercise for functions

---
+++ modern_javascript_exercises/functions/functions.js
@@ -0,0 +1,27 @@
+// Function Declarations
+// logPerson() is a "Function Declaration"
+// Note how it uses the "function" keyword at the beginning of it's declaration
+// This is the original way of writing functions in JavaScript
+
+const people = ["Harry", "Hermione", "Ron"];
+
+function logPerson(person) {
+  console.log("The person is " + person);
+}
+
+people.forEach(logPerson);
+
+// Function expressions
+// Refactor logPerson() so it is becomes a function expression
+
+// Your code here
+
+// Anonymous Functions
+// Refactor logPerson() so it is becomes an anonymous function
+
+// Your code here
+
+// Anonymous Arrow Functions
+// Refactor logPerson() so it is becomes a anonymous arrow function
+
+// Your code here
---
+++ modern_javascript_exercises/functions/functions.md
@@ -0,0 +1,13 @@
+# Functions
+
+In JavaScript here are many different ways to write functions.
+
+In `functions.js`, refactor the `logPerson()` function declaration, into one of each of the following functions.
+
+- Function expression
+- Anonymous Function
+- Anonymous Arrow Function
+
+Arrow functions are a more modern way of writing JavaScript functions.
+
+Note: There are no tests for this task!
