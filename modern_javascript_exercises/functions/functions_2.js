1518d3e505528664320095001a02799256cdd679
Ellie Judge <judge_ellie@hotmail.com>
2022-11-17 16:17:47 +0000

    fixes formatting in functions_2

--- modern_javascript_exercises/functions/functions_2.js
+++ modern_javascript_exercises/functions/functions_2.js
@@ -1,27 +1,21 @@
 // Instructions to this exercise can be found in functions_2.md
 
 // Function Declarations ⭐️
-​
-const people = ["Heather", "Barry", "John", "Maria"];​
 
-function logPerson(person) {​
-  console.log("The person is " + person);​
+const people = ["Heather", "Barry", "John", "Maria"];
+
+function logPerson(person) {
+  console.log("The person is " + person);
 }
 
 people.forEach(logPerson);
 
-
-
-
-
 // Functions as a Value ⭐️
 
-const pets = ["Twinkle", "Toby", "Mr Snuggles", "Hissy McGee"];​
+const pets = ["Twinkle", "Toby", "Mr Snuggles", "Hissy McGee"];
 
-const logPet = function(pet) {​
-  console.log("The pet is " + pet);​
-}
+const logPet = function (pet) {
+  console.log("The pet is " + pet);
+};
 
 pets.forEach(logPet);
-
-
